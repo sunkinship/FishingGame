74f00a2cea67312d937767539761932fbaf9fa44
Trentii <wegenertrent@gmail.com>
2024-03-02 23:46:00 -0800

    UI and some anims

---
+++ Assets/Scripts/UI/UIAnimation.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UIAnimation : MonoBehaviour
+{
+
+    public Image ui_Image;
+
+    public Sprite[] ui_spriteArray;
+
+    public float speed = 0.2f;
+
+    private int indexSprite;
+
+    bool hasRun = false;
+
+    private void Update()
+    {
+        if(hasRun == false)
+        {
+            StartCoroutine(PlayAnim());
+        }
+        
+    }
+
+    IEnumerator PlayAnim()
+    {
+        hasRun = true;
+
+        yield return new WaitForSeconds(speed);
+
+        if (indexSprite >= ui_spriteArray.Length)
+        {
+            indexSprite = 0;
+        }
+        ui_Image.sprite = ui_spriteArray[indexSprite];
+        indexSprite += 1;
+       
+    }
+}
